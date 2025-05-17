8ec7f8939539780984201cae3243d20e6b2b63c3
imanuel <androidmod179@gmail.com>
2025-05-17 17:57:53 +0700

    Remove conflicting login pages and fix route issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from '@/App';
-import '@/index.css';
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Import halaman
+import LoginPage from "./pages/login";
+import AdminDashboard from "./pages/admin";
+import ProductAdminPage from "./pages/admin/products";
+import BlogAdminPage from "./pages/admin/blog";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/products" element={<ProductAdminPage />} />
+        <Route path="/admin/blog" element={<BlogAdminPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
